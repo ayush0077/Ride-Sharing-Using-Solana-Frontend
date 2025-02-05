4bae3ba3f6c05bd6bbb4277799b6f197a3ace747
ayush0077 <ayush.tamrakar.90@gmail.com>
2025-02-05 12:00:00 +0545

    he

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/auth_provider.dart';
+import 'screens/drivermap_screen.dart'; // Driver Map Screen
+import 'screens/ridermap_screen.dart'; // Rider Map Screen
+import 'screens/login_screen.dart'; // Login Screen
+import 'screens/registration_screen.dart'; // Registration Screen
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ride Sharing',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'NotoSansDevanagari', // ✅ Set global font for the entire app
+        ),
+        initialRoute: '/login', // Set initial route to login
+        routes: {
+          '/login': (context) => const LoginScreen(),
+          '/register': (context) => const RegistrationScreen(),
+          '/driverMap': (context) => const DriverMapScreen(),
+          '/riderMap': (context) => const RiderMapScreen(),
+        },
+      ),
+    );
+  }
+}
